dbce9f9d507c057bd1bee660b1be8fb4970fddb9
mac <ganeshmani009@gmail.com>
2020-08-15 13:32:25 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import data from "./data.json";
 function App() {
+  const [selectedColor, setSelectedColor] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="w-1/2 h-16 mx-auto flex justify-around items-center">
+        <div
+          className="rounded"
+          style={{
+            width: "40px",
+            height: "20px",
+            backgroundColor: selectedColor && selectedColor.hexString,
+          }}
+        ></div>
+
+        <div className="text-textcolor">
+          HEX :{" "}
+          <span className="text-hexcolor">
+            {selectedColor && selectedColor.hexString}
+          </span>
+        </div>
+        <div className="text-textcolor">
+          RGB :{" "}
+          <span className="text-hexcolor">
+            {selectedColor &&
+              `${selectedColor.rgb.r},${selectedColor.rgb.g},${selectedColor.rgb.b}`}
+          </span>
+        </div>
+        <div className="text-textcolor">
+          HSL :{" "}
+          <span className="text-hexcolor">
+            {selectedColor &&
+              `${Math.round(selectedColor.hsl.h)},${selectedColor.hsl.s}%,${
+                selectedColor.hsl.l
+              }%`}
+          </span>
+        </div>
+      </div>
+      <div className="grid rounded-lg  grid-cols-6 w-1/2 mx-auto overflow-y-scroll content-area">
+        {data.map((item, index) => {
+          return (
+            <div
+              className="w-full rounder-lg h-12 transition duration-150 ease-in-out transform hover:scale-150 card"
+              style={{
+                backgroundColor: item.hexString,
+              }}
+              onClick={() => {
+                setSelectedColor(item);
+              }}
+              dataHex={item.hexString}
+            ></div>
+          );
+        })}
+      </div>
     </div>
   );
 }
