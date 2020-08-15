362831b699ecfbc078d7c8c542817945d616b206
mac <ganeshmani009@gmail.com>
2020-08-15 13:32:08 +0530

    Updates

---
+++ precache-manifest.eea065a6bdd5bcedc1f5982ede6fb4cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16e72e10e9c019ad4eb654a1a002817",
+    "url": "/rgbcolor-chart/index.html"
+  },
+  {
+    "revision": "0e6bde4deb9a88f10103",
+    "url": "/rgbcolor-chart/static/css/main.2c136496.chunk.css"
+  },
+  {
+    "revision": "79160c47abf90e969f75",
+    "url": "/rgbcolor-chart/static/js/2.27e659df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rgbcolor-chart/static/js/2.27e659df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e6bde4deb9a88f10103",
+    "url": "/rgbcolor-chart/static/js/main.390c6d82.chunk.js"
+  },
+  {
+    "revision": "b38da717f575b7c4ca06",
+    "url": "/rgbcolor-chart/static/js/runtime-main.5cadc4b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rgbcolor-chart/static/media/logo.5d5d9eef.svg"
+  }
+]);
